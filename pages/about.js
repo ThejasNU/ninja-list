7ef5261035f8e4418342f332ed18382a70316a27
ThejasNU <thejasnu@gmail.com>
2021-12-25 03:36:12 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+					vero fugiat fuga, ipsam necessitatibus, iste voluptatem
+					nobis labore ipsum atque libero eius odio neque architecto!
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+					vero fugiat fuga, ipsam necessitatibus, iste voluptatem
+					nobis labore ipsum atque libero eius odio neque architecto!
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+					vero fugiat fuga, ipsam necessitatibus, iste voluptatem
+					nobis labore ipsum atque libero eius odio neque architecto!
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default About;
